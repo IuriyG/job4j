187733e95bb10eec59d20fde69cc0f413cc298ac
Yuriy <CB.Phantom@ya.ru>
2019-11-24 14:14:59 +0300

    Выполнение задачи: 4.5. Отладка программы в IDEA[#192040]

--- chapter_001/src/main/java/ru/job4j/condition/SqArea.java
+++ chapter_001/src/main/java/ru/job4j/condition/SqArea.java
@@ -1,21 +1,27 @@
 package ru.job4j.condition;
 
 public class SqArea {
-    public static double square(int p, int k) {
+    public static int square(int p, int k) {
         /*
-        int x = 1;
+        int x = 1;                                    // Вариант№1, решение для задания: 3.8 Площадь треугольника.[#192082]
         int P = (k * x) + (k * x) + x + x;
         int h = P / p;
         int L = h * k;
         return h * L;
          */
-        return Math.pow(p / (2 * (k + 1)), 2) * k;
-    }
+        //return Math.pow(p / (2 * (k + 1)), 2) * k; // Вариант№2, решение для задания: 3.8 Площадь треугольника.[#192082]
 
-    public static void main(String[] args) {
+        int h = p / (2 * (k + 1));
+        int w = k * h;
+        int s = w * h;
+        return s;
+    }
+/*
+    public static void main(String[] args) {        // Необходимо для выполнения условия задания: 3.8 Площадь треугольника.[#192082]
         double result = square(4, 1);
         double result1 = square(6, 2);
         System.out.println(" p = 4, k = 1, s = 1, real = " + result);
         System.out.println(" p = 6, k = 2, s = 2, real = " + result1);
     }
+ */
 }
--- chapter_001/src/test/java/ru/job4j/condition/SqAreaTest.java
+++ chapter_001/src/test/java/ru/job4j/condition/SqAreaTest.java
@@ -1,16 +1,36 @@
 package ru.job4j.condition;
 
-import org.junit.Assert;
 import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
 
 public class SqAreaTest {
 
+    /* @Test                  // Тест для задания: 3.8 Площадь треугольника.[#192082]
+     public void square() {
+         int in = 6;
+         int in2 = 2;
+         int expected = 2;
+         double out = SqArea.square(in, in2);
+         Assert.assertEquals(expected, out, 0.01);
+     }
+      */
     @Test
-    public void square() {
-        int in = 6;
-        int in2 = 2;
+    public void whenp4k1s1() {
+        int k = 1;
+        int p = 4;
+        int expected = 1;
+        int s = SqArea.square(p, k);
+        assertThat(s, is(expected));
+    }
+
+    @Test
+    public void whenp6k2s2() {
+        int k = 2;
+        int p = 6;
         int expected = 2;
-        double out = SqArea.square(in, in2);
-        Assert.assertEquals(expected, out, 0.01);
+        int s = SqArea.square(p, k);
+        assertThat(s, is(expected));
     }
 }
