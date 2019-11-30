c99e2a253716530ca676f9f72a3fdee0f06a6996
Yuriy <CB.Phantom@ya.ru>
2019-11-30 23:03:34 +0300

    Выполнение задачи: 5.6. Простые числа.[#192050] (Добавил менее сложный вариант)

--- chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -17,3 +17,17 @@ public class PrimeNumber {
         return count;
     }
 }
+/*
+//  Более простой вариант
+public class PrimeNumber {
+    public int calc(int finish) {
+        int count = 0;
+        for (int i = 2; i <= finish; i++) {
+            CheckPrimeNumber checkPrimeNumber = new CheckPrimeNumber();
+            if (checkPrimeNumber.check(i)) count++;
+        }
+        return count;
+    }
+}
+ */
+
