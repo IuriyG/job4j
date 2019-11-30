e9f9ad026b4daada475bd853983627a3fb04b6df
Yuriy <CB.Phantom@ya.ru>
2019-11-30 12:34:50 +0300

    Выполнение задачи: 5.6. Простые числа.[#192050]

---
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -0,0 +1,19 @@
+package ru.job4j.loop;
+
+public class PrimeNumber {
+    public int calc(int finish) {
+        int count = 0;
+        boolean number;
+        for (int i = 2; i <= finish; i++) {
+            number = true;
+            for (int j = 2; j <= i / j; j++)
+                if (i % j == 0) {
+                    number = false;
+                    break;
+                }
+
+            if (number) count++;
+        }
+        return count;
+    }
+}
