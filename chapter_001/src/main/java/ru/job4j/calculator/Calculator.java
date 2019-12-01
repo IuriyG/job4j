aa91752a14900d1b121fcafc1e481160ec086184
Yuriy <CB.Phantom@ya.ru>
2019-12-01 21:34:46 +0300

    Выполнение задачи: 5.8. JavaDoc. Code convention.

--- chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
+++ chapter_001/src/main/java/ru/job4j/calculator/Calculator.java
@@ -1,26 +1,55 @@
 package ru.job4j.calculator;
 
+/**
+ * Class Calculator Класс для работы с арифметическими операциями: сложения, вычитание, умножение и деление.
+  * @author IuriyG
+ */
 public class Calculator {
-    public static void add(double fisrt, double second) {
-        double result = fisrt + second;
-        System.out.println(fisrt + " + " + second + " = " + result);
+    /**
+     * Медот выполняет сложение первого, второго аргумента и выводит результат.
+     * @param first  первый аргумент
+     * @param second второй аргумент
+     */
+    public static void add(double first, double second) {
+        double result = first + second;
+        System.out.println(first + " + " + second + " = " + result);
     }
 
+    /**
+     * Медот выполняет деление первого аргумента на второй и выводит результат.
+     * @param first  первый аргумент
+     * @param second второй аргумент
+     */
     public static void div(double first, double second) {
         double result = first / second;
         System.out.println(first + " / " + second + " = " + result);
     }
 
+    /**
+     * Медот выполняет умножение первого аргумента на второй и выводит результат.
+     * @param first  первый аргумент
+     * @param second второй аргумент
+     */
     public static void multiply(double first, double second) {
         double result = first * second;
         System.out.println(first + " * " + second + " = " + result);
     }
 
+    /**
+     * Медот выполняет вычитание из первого аргумента второго и выводит результат.
+     * @param first  первый аргумент
+     * @param second второй аргумент
+     */
     public static void subtrack(double first, double second) {
         double result = first - second;
         System.out.println(first + " * " + second + " = " + result);
     }
 
+    /**
+     * Main.
+     * @param args args
+     * Вызов методов: сложения, вычитание, умножение и деление; с заданными аргументами
+     */
     public static void main(String[] args) {
         add(1, 1);
         div(4, 2);
