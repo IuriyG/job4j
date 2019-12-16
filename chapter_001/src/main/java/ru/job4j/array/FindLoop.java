edb15afad899b2d8067d3098a4524c6fff03a566
Yuriy <CB.Phantom@ya.ru>
2019-12-16 12:27:22 +0300

    Выполнение задачи: Поиск индекса в диапазоне.

--- chapter_001/src/main/java/ru/job4j/array/FindLoop.java
+++ chapter_001/src/main/java/ru/job4j/array/FindLoop.java
@@ -1,6 +1,18 @@
 package ru.job4j.array;
 
 public class FindLoop {
+    public static int indexOf(int[] data, int el, int start, int finish) {
+        int rst = -1;   // если элемента нет в массиве, то возвращаем -1.
+        finish = data.length;
+        for (; start < finish; start++) {
+            if (data[start] == el) {
+                rst = start;
+                break;
+            }
+        }
+        return rst;
+    }
+
     public int indexOf(int[] data, int el) {
         int rst = -1;   // если элемента нет в массиве, то возвращаем -1.
         int index;
