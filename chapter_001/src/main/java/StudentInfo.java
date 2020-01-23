28c1e6221d842c305cbffcc7a5f2255b0c5a35a2
Yuriy <CB.Phantom@ya.ru>
2020-01-23 21:14:57 +0300

    Создание модуля maven.

--- chapter_001/src/main/java/StudentInfo.java
+++ chapter_001/src/main/java/StudentInfo.java
@@ -2,8 +2,5 @@ public class StudentInfo {
     public static void main(String[] args) {
         System.out.println("Iuriy Gaydarzhi");
         System.out.println("15.11.1985");
-
-
-
     }
 }
--- chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
+++ chapter_001/src/main/java/ru/job4j/loop/PrimeNumber.java
@@ -12,7 +12,6 @@ public class PrimeNumber {
                     break;
                 }
             }
-
             if (number) {
                 count++;
             }
