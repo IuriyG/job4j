3c3e4356324ffaaab335dbe30b1554eb6e16b41b
Yuriy <CB.Phantom@ya.ru>
2020-01-30 17:37:08 +0300

    Вызов метода объекта.

---
+++ chapter_001/src/main/java/ru/job4j/oop/Student.java
@@ -0,0 +1,14 @@
+package ru.job4j.oop;
+
+public class Student {
+    public static void main(String[] args) {
+        Student petya = new Student();
+        petya.song();
+        petya.song();
+        petya.song();
+    }
+
+    public void song() {
+        System.out.println("I believe I can fly");
+    }
+}
