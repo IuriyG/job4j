c7bf953bbff74de6fe6fe4dd37852f3916ffef4f
Yuriy <CB.Phantom@ya.ru>
2020-02-26 22:41:06 +0300

    Реализовать класс Tracker.

---
+++ chapter_001/src/main/java/ru/job4j/tracker/Tracker.java
@@ -0,0 +1,110 @@
+package ru.job4j.tracker;
+
+import java.util.Arrays;
+import java.util.Random;
+
+/**
+ * 1. Реализовать класс Tracker. Класс трекер - это обертка над массивом;
+ * 2. В классе должно быть поле private Item[] items = new Item[100];
+ * 3. Данный класс используется, как хранилище для заявок;
+ * 4. В нем должны быть следующие методы:
+ * добавление заявок - public Item add(Item item);
+ * получение списка всех заявок - public Item[] findAll();
+ * получение списка по имени - public Item[] findByName(String key);
+ * получение заявки по id - public Item findById(String id);
+ * 5. На все методы необходимо написать тесты.
+ */
+
+public class Tracker {
+    /**
+     * Массив для хранения заявок.
+     */
+    private Item[] items = new Item[100];
+    /**
+     * Указатель ячейки для новой заявки.
+     */
+    private int position = 0;
+
+    public int getPosition() {
+        return position;
+    }
+
+    /**
+     * Метод добавления заявки в хранилище.
+     *
+     * @param item новая заявка
+     */
+    public Item add(Item item) {
+        item.setId(this.generateId());
+        items[this.position++] = item;
+        return item;
+    }
+
+    /**
+     * Метод генерирует уникальный ключ для заявки.
+     * Так как у заявки нет уникальности полей, имени и описание. Для идентификации нам нужен уникальный ключ.
+     *
+     * @return Уникальный ключ.
+     */
+    private String generateId() {
+        Random rm = new Random();
+        return String.valueOf(rm.nextLong() + System.currentTimeMillis());
+    }
+
+    /**
+     * Метод возвращает копию массива без null элементов.
+     *
+     * @return готовый массив.
+     */
+    public Item[] findAll() {
+        Item[] outItem = new Item[this.items.length];
+        int size = 0;
+        for (int i = 0; i < getPosition(); i++) {
+            Item a = this.items[i];
+            if (a.getName() != null) {
+                outItem[size] = a;
+                size++;
+            }
+        }
+        this.items = Arrays.copyOf(outItem, size);
+        return this.items;
+    }
+
+    /**
+     * Метод сравнивает в цикле все элементы массива с входящим аргументом key. Если есть совпадения копирует в результирующий массив и возвращает его.
+     *
+     * @param key входной аргумент.
+     * @return возвращает массив.
+     */
+    public Item[] findByName(String key) {
+        Item[] outItem = new Item[this.items.length];
+        int size = 0;
+        for (int i = 0; i < getPosition(); i++) {
+            Item b = this.items[i];
+            if (b.getName() != null && b.getName().equals(key)) {
+                outItem[size] = b;
+                size++;
+            }
+        }
+        items = Arrays.copyOf(outItem, size);
+        return items;
+    }
+
+    /**
+     * Метод проверяет все элементы массива, сравнивая id с аргументом String id и озвращает найденный элемент.
+     * Если элемент не найден - возвращает null.
+     *
+     * @param id входной аргумент.
+     * @return возвращает найденый id.
+     */
+    public Item findById(String id) {
+        Item result = null;
+        for (int i = 0; i < getPosition(); i++) {
+            if (items[i].getId().equals(id)) {
+                result = items[i];
+                break;
+            }
+        }
+        return result;
+    }
+}
---
+++ chapter_001/src/test/java/ru/job4j/tracker/TrackerTest.java
@@ -0,0 +1,73 @@
+package ru.job4j.tracker;
+
+import org.junit.Test;
+
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.nullValue;
+import static org.junit.Assert.assertThat;
+
+public class TrackerTest {
+    /**
+     * Тест для метода add.
+     */
+    @Test
+    public void whenAddNewItemThenTrackerHasSameItem() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test1"));
+        Item result = tracker.findById(item.getId());
+        assertThat(result.getName(), is(item.getName()));
+    }
+
+    /**
+     * Тест для метода findAll. Метод возвращает копию массива без null элементов.
+     */
+    @Test
+    public void whenFindAllReturnCopyArrayWithoutNull() {
+        Tracker tracker = new Tracker();
+        Item one = tracker.add(new Item("test1"));
+        Item two = tracker.add(new Item(null));
+        Item three = tracker.add(new Item("test2"));
+        Item four = tracker.add(new Item("test3"));
+        Item five = tracker.add(new Item(null));
+        Item[] items = new Item[]{one, three, four};
+        assertThat(tracker.findAll(), is(items));
+    }
+
+    @Test
+    public void whenFindByNameReturnCopyArray() {
+        Tracker tracker = new Tracker();
+        Item one = tracker.add(new Item("test1"));
+        Item two = tracker.add(new Item(null));
+        Item three = tracker.add(new Item("test1"));
+        Item four = tracker.add(new Item("test3"));
+        Item five = tracker.add(new Item(null));
+        Item[] expected = new Item[]{one, three};
+        assertThat(tracker.findByName("test1"), is(expected));
+    }
+
+    /**
+     * Тест для метода findById, при котором находится входной аргумент в массиве, метод возвращает найденый элемент.
+     */
+    @Test
+    public void whenFindByIdIsTrue() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test1"));
+        Item item2 = tracker.add(new Item(null));
+        Item item3 = tracker.add(new Item("test2"));
+        Item item4 = tracker.add(new Item("test3"));
+        Item item5 = tracker.add(new Item(null));
+        assertThat(tracker.findById(item3.getId()), is(item3));
+    }
+
+    /**
+     * Тест для метода findById, при котором отсутствует входной аргумент в массиве и метод возвращает null.
+     */
+    @Test
+    public void whenFindByIdIsFalse() {
+        Tracker tracker = new Tracker();
+        Item item = tracker.add(new Item("test1"));
+        Item item2 = tracker.add(new Item(null));
+        Item item3 = new Item("test2");
+        assertThat(tracker.findById(item3.getId()), is(nullValue()));
+    }
+}
