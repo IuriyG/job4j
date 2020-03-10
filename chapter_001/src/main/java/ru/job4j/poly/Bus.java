1d85898e9b31134765114043e6da79b0c58b47a9
Yuriy <CB.Phantom@ya.ru>
2020-03-10 12:47:41 +0300

    Интерфейс Input (корректировка).

--- chapter_001/src/main/java/ru/job4j/poly/Bus.java
+++ chapter_001/src/main/java/ru/job4j/poly/Bus.java
@@ -1,19 +1,40 @@
 package ru.job4j.poly;
 
+/**
+ * 1. Создайте интерфейс транспорт ru.job4j.poly.Transport;
+ * Этот интерфейс будет определять любой вид транспорта.
+ * 2. В интерфейсе объявите три метода: ехать (без параметров, без возвращаемого типа), пассажиры (принимает
+ * число пассажиров, без возвращаемого типа), заправить (принимает кол-во топлива, возвращает цену).
+ * 3. Реализуйте этот интерфейс в классе ru.job4j.poly.Bus.
+ * Внутри методов напишите минимально требуемый код.
+ * 4. Добавьте ссылку с коммитом на эту задачу.
+ */
 public class Bus implements Transport {
+
+    public static void main(String[] args) {
+        Bus bus = new Bus();
+        bus.drive();
+        bus.passengers(55);
+        System.out.println("Минимальная сумма для поездки: " + bus.fill(80) + " руб.");
+    }
 
     @Override
     public void drive() {
+        System.out.println("Сегодня едем на экскурсию.");
 
     }
 
     @Override
     public void passengers(int numbers) {
-
+        if (numbers >= 30 && numbers <= 40) {
+            System.out.println("На автобусе - Shen Long.");
+        } else if (numbers >= 40 && numbers <= 60) {
+            System.out.println("На автобусе - Zonda.");
+        }
     }
 
     @Override
     public int fill(int fuel) {
-        return 0;
+        return fuel * 47;
     }
 }
