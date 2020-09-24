319ebd6d8addc257e38346cbf80af3f4f0cbaf37
Yuriy <CB.Phantom@ya.ru>
2020-09-24 21:57:59 +0300

    Подсчет функции в диапазоне.

---
+++ chapter_004/src/main/java/ru/job4j/lambda/Diapason.java
@@ -0,0 +1,33 @@
+package ru.job4j.lambda;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Function;
+
+/**
+ * 1. Реализовать метод подсчета функции в диапазоне.
+ * List<Double> diapason(int start, int end, Function<Double, Double> func);
+ * 2. Реализации функций в тестах.
+ *     - линейная.
+ *     - квадратичная.
+ *     - показательная. (y = a^x, где a - постоянная)
+ *     Пример:
+ *     "@Test
+ * public void whenLinearFunctionThenLinearResults() {
+ *     List<Double> result = function.diapason(5, 8, x -> 2 * x + 1);
+ *     List<Double> expected = Arrays.asList(11D, 13D, 15D);
+ *     assertThat(result, is(expected));
+ * }
+ *     3. Необходимо использовать только встроенные функциональные интерфейсы
+ */
+public class Diapason {
+
+    List<Double> diapason(int start, int end, Function<Double, Double> func) {
+
+        List<Double> list = new ArrayList<>();
+        for (int i = start; i < end; i++) {
+            list.add(func.apply((double) i));
+        }
+        return list;
+    }
+}
---
+++ chapter_004/src/test/java/ru/job4j/lambda/DiapasonTest.java
@@ -0,0 +1,37 @@
+package ru.job4j.lambda;
+
+import org.junit.Test;
+
+import java.util.Arrays;
+import java.util.List;
+
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.*;
+
+public class DiapasonTest {
+
+    @Test
+    public void whenLinearFunctionThenLinearResults() {
+        Diapason function = new Diapason();
+        List<Double> result = function.diapason(5, 8, x -> 2 * x + 1);
+        List<Double> expected = Arrays.asList(11D, 13D, 15D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenQuadraticFunctionThenQuadraticResults() {
+        Diapason function = new Diapason();
+        List<Double> result = function.diapason(5, 8, x -> 2 * (x * x) + 2 * x + 1);
+        List<Double> expected = Arrays.asList(61D, 85D, 113D);
+        assertThat(result, is(expected));
+    }
+
+    @Test
+    public void whenIndicativeFunctionThenIndicativeResults() {
+        Diapason function = new Diapason();
+        List<Double> result = function.diapason(5, 8, x -> Math.pow(2, x));
+        List<Double> expected = Arrays.asList(32D, 64D, 128D);
+        assertThat(result, is(expected));
+    }
+}
