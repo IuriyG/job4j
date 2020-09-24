5f9081929a906e939619b038ea469da45973937f
Yuriy <CB.Phantom@ya.ru>
2020-09-24 16:53:55 +0300

    Ссылки на методы.

--- README.md
+++ README.md
@@ -1,7 +1,7 @@
 [![Build Status](https://travis-ci.org/IuriyG/job4j.svg?branch=master)](https://travis-ci.org/IuriyG/job4j)
 [![codecov](https://codecov.io/gh/IuriyG/job4j/branch/master/graph/badge.svg)](https://codecov.io/gh/IuriyG/job4j)
 
-# Job4j
+# [Job4j](https://job4j.ru)
 
 Я начал обучение в проекте Job4j!
 Объединение зафиксированных изменений.
---
+++ chapter_004/src/main/java/ru/job4j/lambda/RefMethod.java
@@ -0,0 +1,26 @@
+package ru.job4j.lambda;
+
+import java.util.*;
+import java.util.function.Consumer;
+
+/**
+ * 1. Ниже приведен код. Класс выводит на консоль список имен. Если имя больше 10 символов, то вывод обрезается.
+ */
+public class RefMethod {
+    public static void main(String[] args) {
+        List<String> names = Arrays.asList(
+                "Ivan",
+                "Petr Arsentev"
+        );
+        Consumer<String> out = RefMethod::cutOut;
+        names.forEach(out);
+    }
+
+    public static void cutOut(String value) {
+        if (value.length() > 10) {
+            System.out.println(value.substring(0, 10) + "..");
+        } else {
+            System.out.println(value);
+        }
+    }
+}
