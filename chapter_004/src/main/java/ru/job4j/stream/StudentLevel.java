c36b22fac65100d277baf5b06767394d990b2e67
Yuriy <CB.Phantom@ya.ru>
2020-10-08 14:54:44 +0300

    Stream API улучшения (Stream.takeWhile, Stream.dropWhile, Stream.ofNullable).

---
+++ chapter_004/src/main/java/ru/job4j/stream/StudentLevel.java
@@ -0,0 +1,25 @@
+package ru.job4j.stream;
+
+import java.util.Comparator;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+/**
+ * <b>Задание.</b>
+ * 1. Реализовать фильтрацию учеников по проходному баллу.
+ * name - ФИО студента, score - бал аттестата.
+ * Необходимо реализовать метод List<Student> levelOf(List<Student> students, int bound);
+ * Метод должен вернуть список студентов, у которых балл аттестата больше bound.
+ * Во входящем списке могут быть null элементы.
+ * Порядок действий.
+ * - Используя метод filter убрать null;
+ * - Отсортировать список;
+ * - Используя метод takeWhile, получить нужный поток;
+ * - Сохранить поток в List.
+ */
+public class StudentLevel {
+    public static List<Student> levelOf(List<Student> students, int bound) {
+        return students.stream().filter(Objects::nonNull).sorted(Comparator.comparingInt(Student::getScore).reversed()).takeWhile(st -> st.getScore() > bound).collect(Collectors.toList());
+    }
+}
