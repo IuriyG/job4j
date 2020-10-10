5e5fa2c4257e3a2e993af74b1e931a560ee0e6e4
Yuriy <CB.Phantom@ya.ru>
2020-10-10 12:30:10 +0300

    Local-Variable Type Inference.

--- chapter_003/src/test/java/ru/job4j/search/PhoneDictionaryTest.java
+++ chapter_003/src/test/java/ru/job4j/search/PhoneDictionaryTest.java
@@ -8,41 +8,41 @@ import java.util.ArrayList;
 public class PhoneDictionaryTest {
     @Test
     public void whenFindByName() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
-        ArrayList<Person> persons = phones.find("Petr");
+        var persons = phones.find("Petr");
         Assert.assertEquals(persons.get(0).getSurname(), ("Arsentev"));
     }
 
     @Test
     public void whenFindByPartOfSurname() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
-        ArrayList<Person> persons = phones.find("tev");
+        var persons = phones.find("tev");
         Assert.assertEquals(persons.get(0).getSurname(), ("Arsentev"));
     }
 
     @Test
     public void whenFindByPartOfPhone() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
-        ArrayList<Person> persons = phones.find("34");
+        var persons = phones.find("34");
         Assert.assertEquals(persons.get(0).getSurname(), ("Arsentev"));
     }
 
     @Test
     public void whenFindByPartOfAddress() {
-        PhoneDictionary phones = new PhoneDictionary();
+        var phones = new PhoneDictionary();
         phones.add(
                 new Person("Petr", "Arsentev", "534872", "Bryansk")
         );
-        ArrayList<Person> persons = phones.find("nsk");
+        var persons = phones.find("nsk");
         Assert.assertEquals(persons.get(0).getSurname(), ("Arsentev"));
     }
 }
