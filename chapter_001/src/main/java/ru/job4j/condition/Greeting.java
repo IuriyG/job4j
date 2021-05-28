b4b6e6c3fe3da680feb7254db157c3ec6443ee7a
Yuriy <CB.Phantom@ya.ru>
2021-05-28 22:09:17 +0300

    String

---
+++ chapter_001/src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,21 @@
+package ru.job4j.condition;
+
+/**
+ * @author Iuriy Gaydarzhi.
+ * @since 28.05.2021
+ */
+public class Greeting {
+
+    /**
+     * Метод выводит в консоль значение переменных.
+     *
+     * @param args аргументы.
+     */
+    public static void main(String[] args) {
+
+        String idea = "I like Java!";
+        String noob = " But I am a newbie. ";
+        int year = 2021;
+        System.out.println(idea + noob + year);
+    }
+}
