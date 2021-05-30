20e5b5735a2ec22fa120992b694ec805b2d67903
Yuriy <CB.Phantom@ya.ru>
2021-05-30 22:59:46 +0300

    Что такое import.

---
+++ chapter_001/src/main/java/ru/job4j/calculator/MathCalculator.java
@@ -0,0 +1,31 @@
+package ru.job4j.calculator;
+
+import static ru.job4j.math.MathFunction.*;
+
+/**
+ * @author Iuriy Gaydarzhi.
+ * @since 30.05.2021
+ */
+public class MathCalculator {
+
+    public static double sumAndMultiply(double first, double second) {
+        return sum(first, second)
+                + multiply(first, second);
+    }
+
+    public static double minusAndDivision(double first, double second) {
+        return minus(first, second)
+                + division(first, second);
+    }
+
+    public static double sumAllOperation(double first, double second) {
+        return sumAndMultiply(first, second) + minus(first, second)
+                + division(first, second);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Результат расчета равен: " + sumAndMultiply(10, 20));
+        System.out.println("Результат расчета равен: " + minusAndDivision(10, 20));
+        System.out.println("Результат расчета равен: " + sumAllOperation(10, 20));
+    }
+}
---
+++ chapter_001/src/main/java/ru/job4j/math/MathFunction.java
@@ -0,0 +1,25 @@
+package ru.job4j.math;
+
+/**
+ * @author Iuriy Gaydarzhi.
+ * @since 30.05.2021
+ */
+public class MathFunction {
+
+    public static double sum(double first, double second) {
+        return first + second;
+    }
+
+    public static double multiply(double first, double second) {
+        return first * second;
+    }
+
+    public static double minus(double first, double second) {
+        return first - second;
+    }
+
+    public static double division(double first, double second) {
+        return first / second;
+    }
+
+}
