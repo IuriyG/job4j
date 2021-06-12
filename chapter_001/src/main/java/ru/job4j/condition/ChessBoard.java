dd1983b874b0ab6966c90c94dd7a07ab8bbbe4a1
Yuriy <CB.Phantom@ya.ru>
2021-06-12 14:03:07 +0300

    Math.abs (корректировка).

--- chapter_001/src/main/java/ru/job4j/condition/ChessBoard.java
+++ chapter_001/src/main/java/ru/job4j/condition/ChessBoard.java
@@ -47,6 +47,6 @@ public class ChessBoard {
      * @return Возвращает true или false.
      */
     public static boolean coordinatLimit(int x1, int y1, int x2, int y2) {
-        return x2 <= 7 && x1 >= 0 && y2 <= 7 && y1 >= 0;
+        return x2 <= 7 && x2 >= 0 && x1 <= 7 && x1 >= 0 && y2 <= 7 && y2 >= 0 && y1 <= 7 && y1 >= 0;
     }
 }
