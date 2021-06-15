4aa96600313f470d1a1844148f47162d03a17142
Yuriy <CB.Phantom@ya.ru>
2021-06-15 23:31:04 +0300

    Сумма чисел.

--- chapter_001/src/main/java/ru/job4j/loop/Counter.java
+++ chapter_001/src/main/java/ru/job4j/loop/Counter.java
@@ -10,4 +10,31 @@ public class Counter {
         }
         return sum;
     }
+
+    /**
+     * Метод суммирует значение чисел.
+     * @param start Суммирование начинается с этой переменной.
+     * @param finish Суммирование завершается включая эту.
+     * @return Сумма чисел.
+     */
+    public static int sum(int start, int finish) {
+        int sum = 0;
+        for (; start <= finish; start++) {
+            sum = sum + start;
+        }
+        return sum;
+    }
+
+    /**
+     * Main.
+     * @param args Входящий аргумент.
+     */
+    public static void main(String[] args) {
+        System.out.println(sum(0, 5));
+        System.out.println(sum(0, 10));
+        System.out.println(sum(3, 8));
+        System.out.println(sum(1, 1));
+        System.out.println(sum(2, 12));
+        System.out.println(sum(0, 16));
+    }
 }
