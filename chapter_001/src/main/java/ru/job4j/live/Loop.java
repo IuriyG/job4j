905e47d661ed1ba4fa26f7421f6c20965bda78c6
Yuriy <CB.Phantom@ya.ru>
2021-06-16 21:36:10 +0300

    Генерация конструкций в IDEA.

---
+++ chapter_001/src/main/java/ru/job4j/live/Loop.java
@@ -0,0 +1,15 @@
+package ru.job4j.live;
+
+/**
+ * @author Iuriy Gaydarzhi.
+ * @since 16.06.2021
+ */
+
+//Демонстрация работы живого шаблона постфикса.
+public class Loop {
+    public static void main(String[] args) {
+        for (int i = 0; i < 10; i++) {
+            System.out.println("Hello postfix)");
+        }
+    }
+}
