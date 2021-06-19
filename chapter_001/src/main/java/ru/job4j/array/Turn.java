fa4afc1ecbed527a2bb2fd334da6076b46ac9738
Yuriy <CB.Phantom@ya.ru>
2021-06-19 23:23:10 +0300

    Перевернуть массив.

--- chapter_001/src/main/java/ru/job4j/array/Turn.java
+++ chapter_001/src/main/java/ru/job4j/array/Turn.java
@@ -1,6 +1,12 @@
 package ru.job4j.array;
 
 public class Turn {
+    /**
+     * Метод переворачивает массив задом наперёд.
+     *
+     * @param array Входящий массив.
+     * @return Перевернутый массив.
+     */
     public int[] back(int[] array) {
         for (int i = 0; i < array.length / 2; i++) {
             int temp = array[i];
