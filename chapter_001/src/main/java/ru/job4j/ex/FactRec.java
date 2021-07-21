7365f3d16a0453330ca648b5e1fbb6733a063532
Yuriy <CB.Phantom@ya.ru>
2021-07-21 23:33:39 +0300

    Error - исключения, связанные с работой виртуальной машины.

---
+++ chapter_001/src/main/java/ru/job4j/ex/FactRec.java
@@ -0,0 +1,33 @@
+package ru.job4j.ex;
+
+/**
+ * @author Iuriy Gaydarzhi.
+ * @since 21.07.2021
+ *
+ * 1. Запустите код вычисления факториала через рекурсию.
+ */
+public class FactRec {
+    /**
+     * Метод вычисляет факториала через рекурсию.
+     *
+     * @param n Входящее число.
+     * @return Результат.
+     */
+    public static int calc(int n) {
+        if (n == 0 || n == 1) {
+            return 1;
+        } else {
+            return calc(n - 1) * n;
+        }
+    }
+
+    /**
+     * Main.
+     *
+     * @param args Входящие аргументы.
+     */
+    public static void main(String[] args) {
+        int rsl = calc(3);
+        System.out.println(rsl);
+    }
+}
