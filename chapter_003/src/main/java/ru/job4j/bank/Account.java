a0e8f6d038ecff9538e634f67c93783767e19dd1
Yuriy <CB.Phantom@ya.ru>
2021-08-30 23:06:39 +0300

    Что такое JavaDoc.

--- chapter_003/src/main/java/ru/job4j/bank/Account.java
+++ chapter_003/src/main/java/ru/job4j/bank/Account.java
@@ -2,31 +2,77 @@ package ru.job4j.bank;
 
 import java.util.Objects;
 
+/**
+ * Класс является моделью данных банковского счета для банковского сервиса.
+ *
+ * @author Iuriy Gaydarzhi.
+ * @since 29.08.2020
+ */
 public class Account {
+
+    /**
+     * Поле реквизитов пользователя.
+     */
     private String requisite;
+
+    /**
+     * Поле Баланса пользователя.
+     */
     private double balance;
 
+    /**
+     * Конструктор.
+     *
+     * @param requisite Входящее поле реквизитов пользователя.
+     * @param balance   Входящее поле баланса пользователя.
+     */
     public Account(String requisite, double balance) {
         this.requisite = requisite;
         this.balance = balance;
     }
 
+    /**
+     * Геттер для поля реквизиты пользователя.
+     *
+     * @return Реквизиты пользователя.
+     */
     public String getRequisite() {
         return requisite;
     }
 
+    /**
+     * Сеттер для поля реквизиты пользователя.
+     *
+     * @param requisite Реквизиты пользователя.
+     */
     public void setRequisite(String requisite) {
         this.requisite = requisite;
     }
 
+    /**
+     * Геттер для поля баланса пользователя.
+     *
+     * @return Баланс пользователя.
+     */
     public double getBalance() {
         return balance;
     }
 
+    /**
+     * Сеттер для поля баланса пользователя.
+     *
+     * @param balance Баланс пользователя.
+     */
     public void setBalance(double balance) {
         this.balance = balance;
     }
 
+    /**
+     * Переопределенный метод сравнения поля реквизиты пользователя.
+     *
+     * @param o Входящий объект сравнения.
+     * @return Результат сравнения.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -39,6 +85,11 @@ public class Account {
         return Objects.equals(requisite, account.requisite);
     }
 
+    /**
+     * Переопределенный метод hashcode для поля реквизиты пользователя.
+     *
+     * @return Значение хеш-кода для поля реквизиты.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(requisite);
--- chapter_003/src/main/java/ru/job4j/bank/User.java
+++ chapter_003/src/main/java/ru/job4j/bank/User.java
@@ -2,31 +2,77 @@ package ru.job4j.bank;
 
 import java.util.Objects;
 
+/**
+ * Класс является моделью данных пользователя для банковского сервиса.
+ *
+ * @author Iuriy Gaydarzhi.
+ * @since 29.08.2020
+ */
 public class User {
+
+    /**
+     * Поле номер паспорта пользователя.
+     */
     private String passport;
+
+    /**
+     * Поле ФИО пользователя.
+     */
     private String username;
 
+    /**
+     * Конструктор.
+     *
+     * @param passport Входящее поле номера паспорта пользователя.
+     * @param username Входящее поле ФИО пользователя.
+     */
     public User(String passport, String username) {
         this.passport = passport;
         this.username = username;
     }
 
+    /**
+     * Геттер для поля номер паспорта пользователя.
+     *
+     * @return Номер паспорта пользователя.
+     */
     public String getPassport() {
         return passport;
     }
 
+    /**
+     * Сеттер для поля номер паспорта пользователя.
+     *
+     * @param passport Номер паспорта пользователя.
+     */
     public void setPassport(String passport) {
         this.passport = passport;
     }
 
+    /**
+     * Геттер для поля ФИО пользователя.
+     *
+     * @return ФИО пользователя.
+     */
     public String getUsername() {
         return username;
     }
 
+    /**
+     * Сеттер для поля ФИО пользователя.
+     *
+     * @param username ФИО пользователя.
+     */
     public void setUsername(String username) {
         this.username = username;
     }
 
+    /**
+     * Переопределенный метод сравнения поля номер паспорта пользователя.
+     *
+     * @param o Входящий объект сравнения.
+     * @return Результат сравнения.
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -39,6 +85,11 @@ public class User {
         return Objects.equals(passport, user.passport);
     }
 
+    /**
+     * Переопределенный метод hashcode для поля номер паспорта пользователя.
+     *
+     * @return Значение хеш-кода для поля номер паспорта пользователя.
+     */
     @Override
     public int hashCode() {
         return Objects.hash(passport);
