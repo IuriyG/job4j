7f993fad2e6b1db24654b563bee53ff5ce55882d
Yuriy <CB.Phantom@ya.ru>
2021-08-30 22:17:55 +0300

    JavaDoc.

--- chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
+++ chapter_003/src/main/java/ru/job4j/search/PriorityQueue.java
@@ -3,33 +3,39 @@ package ru.job4j.search;
 import java.util.LinkedList;
 
 /**
- * 1. Реализуйте метод put в классе PriorityQueue.
- * 2. Залейте код в репозиторий. Оставьте ссылку на коммит в этой задаче.
- * 3. Выставите ответственного
+ * Класс описывает работу простейшей очереди по приоритету,
+ * которая работает по принципу FIFO (first in - first out).
+ *
+ * @author Iuriy Gaydarzhi.
+ * @version 1.0
  */
 
 public class PriorityQueue {
-    private LinkedList<Task> tasks = new LinkedList<>();
+    private final LinkedList<Task> tasks = new LinkedList<>();
 
     /**
-     * Метод должен вставлять в нужную позицию элемент.
-     * Позиция определяется по полю приоритет.
-     * Для вставки использовать add(int index, E value)
+     * Метод принимает заявку и добавляет ее в очередь.
+     * Если встречаются две заявки с одинаковым полем приоритет,
+     * то в очереди они распределяются по принципу FIFO.
      *
-     * @param task задача
+     * @param task задача, которая добавляется в очередь.
      */
     public void put(Task task) {
         var index = 0;
         for (var element : tasks) {
-            if (task.getPriority() <= element.getPriority()) {
+            if (task.getPriority() < element.getPriority()) {
                 break;
-            } else {
-                index++;
             }
+            index++;
         }
         this.tasks.add(index, task);
     }
 
+    /**
+     * Метод возвращает первую задачу в очереди.
+     *
+     * @return Возвращает первую задачу в очереди или null если очередь пуста.
+     */
     public Task take() {
         return tasks.poll();
     }
