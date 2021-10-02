7c21c1feb2ed3a70f09f1e756edebe63a47af56e
Yuriy <CB.Phantom@ya.ru>
2021-10-02 21:00:33 +0300

    Лямбда блок (корректировка).

--- chapter_004/src/main/java/ru/job4j/lambda/LambdaUsage.java
+++ chapter_004/src/main/java/ru/job4j/lambda/LambdaUsage.java
@@ -4,15 +4,22 @@ import java.util.Arrays;
 import java.util.Comparator;
 
 /**
+ * В классе приводится пример использования лямбды с выводом отладочной инфы на консоль.
+ *
  * @author Iuriy Gaydarzhi.
  * @since 02.10.2021
  */
 public class LambdaUsage {
 
+    /**
+     * Метод сравнивает длину строк.
+     *
+     * @param args Входящий аргумент.
+     */
     public static void main(String[] args) {
         Comparator<String> comparator = (left, right) -> {
             System.out.println("compare - " + right.length() + " : " + left.length());
-            return right.length() - left.length();
+            return Integer.compare(right.length(), left.length());
         };
         String[] str = new String[]{
                 "A",
