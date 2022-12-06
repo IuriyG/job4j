09dc70d31f7528f35117d869d2dbe904653aee1a
Iuriy <iuriyG@proton.me>
2022-12-05 20:54:51 -0800

    Добавил ссылку.

--- chapter_001/src/main/java/ru/job4j/array/JavaNameValidator.java
+++ chapter_001/src/main/java/ru/job4j/array/JavaNameValidator.java
@@ -1,10 +1,18 @@
 package ru.job4j.array;
 
 /**
+ * The type Java name validator.
+ *
  * @author Iuriy Gaydarzhi.
  * @since 16.11.2022
  */
 public class JavaNameValidator {
+    /**
+     * Is name valid boolean.
+     *
+     * @param name the name
+     * @return the boolean
+     */
     public static boolean isNameValid(String name) {
         if (name.isEmpty() || Character.isUpperCase(name.codePointAt(0))
                 || Character.isDigit(name.codePointAt(0))) {
@@ -20,14 +28,32 @@ public class JavaNameValidator {
         return true;
     }
 
+    /**
+     * Is special symbol boolean.
+     *
+     * @param code the code
+     * @return boolean
+     */
     public static boolean isSpecialSymbol(int code) {
         return code == 36 || code == 95;
     }
 
+    /**
+     * Is upper latin letter boolean.
+     *
+     * @param code the code
+     * @return the boolean
+     */
     public static boolean isUpperLatinLetter(int code) {
         return code < 65 || code > 90;
     }
 
+    /**
+     * Is lower latin letter boolean.
+     *
+     * @param code the code
+     * @return boolean
+     */
     public static boolean isLowerLatinLetter(int code) {
         return code < 97 || code > 122;
     }
